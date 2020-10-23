4488817113e52f6924e0dcd899d817461f5f856d
Maciej Lagowski <m_lagowski@o2.pl>
2020-10-23 16:03:26 +0200

    Initial commit

---
+++ src/main/java/io/github/maciejlagowski/airfield/AirfieldApplication.java
@@ -0,0 +1,12 @@
+package io.github.maciejlagowski.airfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirfieldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirfieldApplication.class, args);
+    }
+}
